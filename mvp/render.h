410c63172fb5679256d431e27d7904976aa3ede8
canigou101 <127309239+canigou101@users.noreply.github.com>
2026-04-08 19:13:38 +0200

    cleanup

--- mvp/render.h
+++
@@ -1,9 +0,0 @@
-#ifndef RENDER_H
-#define RENDER_H
-
-#include "t_noeud.h"
-
-/* Fonction principale */
-void render(t_noeud *racine);
-
-#endif
--- test/Rendu.h
+++
@@ -1,8 +0,0 @@
-#ifndef RENDU_H
-#define RENDU_H
-
-#include "Arbre.h"
-
-void afficher_nanoml(t_noeud* racine);
-
-#endif
